44c063f62e6ce914f6a2eb0a69e568ce0ad56939
Yevhenii Tomberg <yevhenii@tomberg.com>
2025-10-23 23:00:44 +0300

    Added 3 new animals: Horse, Bear, Duck

---
+++ Klassid/Bear.cs
@@ -0,0 +1,33 @@
+﻿using System.Media;
+
+namespace LoomamaaApp.Klassid
+{
+    public class Bear : Animal, ICrazyAction
+    {
+        public Bear(string name, int age) : base(name, age) { }
+
+        public override string MakeSound()
+        {
+            PlaySound("Sounds/bear.wav");
+            return "Grrrrr!";
+        }
+
+        public string ActCrazy()
+        {
+            return $"{Name} rolled on the ground and scared everyone! ";
+        }
+
+        private void PlaySound(string filePath)
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer(filePath);
+                player.Play();
+            }
+            catch
+            {
+                // ignore missing sound file
+            }
+        }
+    }
+}
